8894a240a80d220c12275fd22da95dcc61753d30
FABRIZIO JOSE RAMOS LOPEZ <fabriziojramos20@unitec.edu>
2023-05-06 11:43:39 -0600

    Muchos Archivos

--- Proyecto_Progra_3/main.cpp
+++ Proyecto_Progra_3/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+
 
 using namespace std;
+using namespace sf;
 
 int main() {
+
+
+
+
 
 	cout << "Aqui se empieza...";
 	cout << "Un Push";
