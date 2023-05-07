aa5b14672b9e5ceb62f19729ec533cc040b00380
FABRIZIO JOSE RAMOS LOPEZ <fabriziojramos20@unitec.edu>
2023-05-07 10:49:42 -0600

    Arreglando cosas

--- Proyecto_Progra_3/main.cpp
+++ Proyecto_Progra_3/main.cpp
@@ -11,8 +11,20 @@ using namespace sf;
 
 int main() {
 
+	int fps = 60;
+	bool gameover= false;
 
 
+
+	RenderWindow* Ventana1;
+	Ventana1 = new RenderWindow(VideoMode(800, 600), "Hola");
+	Ventana1->setFramerateLimit(fps);
+
+	while (!gameover) {
+		Ventana1->clear(Color::Magenta);
+
+		Ventana1->display();
+	}
 
 
 	cout << "Aqui se empieza...";
