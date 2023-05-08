fdac40990129fe198526f3148a7f9cce6bc7ebf2
Kelmel13 <kelvinmelgar777@gmail.com>
2023-05-08 12:32:39 -0600

    Animando Nave - Tiene Errores 1.0

--- Proyecto_Progra_3/Nave.cpp
+++ Proyecto_Progra_3/Nave.cpp
@@ -1,5 +1,5 @@
 #include "Nave.h"
-
+// Para la clase nave
 Nave::Nave(Vector2f& pos_a) {
 
 	textura = new Texture();
@@ -11,19 +11,39 @@ Nave::Nave(Vector2f& pos_a) {
 
 	vel = { 0.f, 0.f };
 	pos = pos_a;
+
+	animations[int(AnimationIndex::LeftMov)] = new Animation(0, 0, 16, 16);
+	animations[int(AnimationIndex::Non)] = new Animation(16, 0, 16, 16);
+	animations[int(AnimationIndex::LeftMov)] = new Animation(32, 0, 16, 16);
 	
 }
 
 void Nave::SetDireccion(const Vector2f& dir)
 {
 	vel = dir * speed;
+	if (dir.x > 0.f) {
+		curAnimation = AnimationIndex::RighMov;
+	}
+	else if (dir.x < 0.f)
+	{
+		curAnimation = AnimationIndex::LeftMov;
+	}
+	else
+	{
+		curAnimation = AnimationIndex::Non;
+	}
 }
 
 void Nave::Update(float dt)
 {
 	pos += vel * dt;
+
+	animations[int( curAnimation)]->Update(dt);
+	animations[int(curAnimation)]->ApplyToSprite(*spr);
+	 
 	spr->setPosition(pos);
 }
+
 
 void Nave::Draw(RenderTarget& rt) const
 {
@@ -31,3 +51,40 @@ void Nave::Draw(RenderTarget& rt) const
 	rt.draw(*spr);
 }
 
+
+// Para la clase de animacion
+
+Animation::Animation(int x, int y, int width, int height)
+{
+	textura.loadFromFile("Assets\\Mini Pixel Pack 3\\Player ship\\Player_ship.png");
+
+	for (int i = 0; i < nFrames; i++)
+	{
+		frames[i] = { x + i * width, y, width, height };
+	}
+}
+
+
+void Animation::ApplyToSprite(Sprite& s) const
+{
+	s.setTexture(textura);
+	s.setTextureRect(frames[iFrame]);
+}
+
+void Animation::Update(float dt)
+{
+	time += dt;
+	while (time >= holdTime)
+	{
+		time -= holdTime;
+		Advance();
+	}
+}
+
+void Animation::Advance()
+{
+	if (++iFrame >= nFrames)
+	{
+		iFrame = 0;
+	}
+}
--- Proyecto_Progra_3/Nave.h
+++ Proyecto_Progra_3/Nave.h
@@ -3,6 +3,35 @@
 
 using namespace sf;
 
+
+class Animation
+{
+public:
+	Animation(int x, int y, int width, int height);
+	Animation() = default;
+
+	void ApplyToSprite(Sprite& s) const;
+
+	void Update(float dt);
+
+private:
+	void Advance();
+
+private:
+	static constexpr int nFrames = 3;
+	static constexpr float holdTime = 0.1f;
+	Texture textura;
+
+	// Los frames los guardamos en un arreglo de rectangulos para que su manipulacion sea mas sencilla
+	IntRect frames[nFrames];
+	// Representa el frame que esta actualmente
+	int iFrame = 2;
+
+	float time = 0.f;
+
+
+};
+
 class Nave
 {	
 private:
@@ -15,6 +44,18 @@ private:
 	Sprite* spr;
 
 
+	enum class AnimationIndex
+	{
+		LeftMov,
+		Non,
+		RighMov,
+		Count
+	};
+
+	Animation* animations[int(AnimationIndex::Count )]; 
+	AnimationIndex curAnimation = AnimationIndex::RighMov;
+
+
 public:
 	Nave(Vector2f& pos_a);
 	~Nave();
@@ -23,5 +64,7 @@ public:
 	void SetDireccion(const Vector2f& dir);
 	void Update(float dt);
 
+
 };
+
 
