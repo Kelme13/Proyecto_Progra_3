dbee384631eb8ece3f67ebe5539a966b3a928b51
FABRIZIO JOSE RAMOS LOPEZ <fabriziojramos20@unitec.edu>
2023-05-10 11:26:22 -0600

    El background ya esta hecho

--- Proyecto_Progra_3/Background.cpp
+++ Proyecto_Progra_3/Background.cpp
@@ -22,7 +22,7 @@ Background::Background(const char* BackgroundFile, const char* EstrellaFile){
 
 	Star = new Sprite(*Estrella);
 	Star->setTexture(*Estrella);
-
+	Star->setPosition(0.f, -6000.f);
 }
 
 void Background::Draw(sf::RenderTarget& rt) const {
@@ -30,21 +30,21 @@ void Background::Draw(sf::RenderTarget& rt) const {
 	sprite->setOrigin(2900.f, 440.f);
 	sprite->rotate(0.01f);
 	sprite->setScale(3, 3);
-	Estrellas(rt);
 	rt.draw(*sprite);
+	Estrellas(rt);
 }
 
 void Background::Estrellas(sf::RenderTarget& rt) const {
 
 
-	Star->setScale(2,2);
-	/*if (Star->getPosition().y > 1000) {
-		Star->setPosition(0, 0);
-	}*/
-	
+	Star->setScale(1,1);
+	if (Star->getPosition().y > 2500.f) {
+		Star->setPosition(0.f, -6000.f);
+	}
+	Star->setColor(sf::Color(255, 255, 255, 128));
 
-	//Star->move(0.f, 0.2f);
-	
+	Star->move(0.f, 0.4f);
+	//Star->rotate(0.003f);
 
 
 
